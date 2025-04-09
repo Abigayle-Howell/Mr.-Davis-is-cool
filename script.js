893197f8a0aac4c94d2b1a4fd44809a9e3596da6
Abigayle <115105929+Abigayle-Howell@users.noreply.github.com>
2025-04-09 10:01:05 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,5 @@
     <title>Mr. Davis is cool</title>
     <link rel="stylesheet" href="style.css">
 </head>
-<body>
-    <div class="container">
-        <h3>Balance This Equation</h3>
-        <p id="equation"> <input type="number" id="coeff1" min="1"> X + 
-           <input type="number" id="coeff2" min="1"> Y → 
-           <input type="number" id="coeff3" min="1"> Z</p>
-        <button onclick="checkBalance()">Check</button>
-        <button onclick="newEquation()">New Equation</button>
-        <p class="result" id="result"></p>
-    </div>
-    <script src="script.js"></script>
 </body>
 </html>
